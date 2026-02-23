34af95caf61cf99e8fa6428bf594f82754e47f38
amiines <amirnesru054@gmail.com>
2026-02-23 14:39:40 +0300

    Update A - Linear Keyboard.py

--- A - Linear Keyboard.py
+++ A - Linear Keyboard.py
@@ -1,1 +1,8 @@
-
+t=int(input())
+for i in range(t):
+    letters=input()
+    s=input()
+    a=0
+    for j in range(len(s)-1):
+        a+=abs(letters.index(s[j+1])-letters.index(s[j]))
+    print(a)    
